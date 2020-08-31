a48f11c5e010f73cb37e5c341c9e643c2fca8fa9
arvisha16 <70545496+arvisha16@users.noreply.github.com>
2020-08-31 16:55:04 -0700

    Create create-flavor.sh
    Script to create flavor

---
+++ create-flavor.sh
@@ -0,0 +1,8 @@
+sudo bash
+
+source openrc
+
+nova flavor-create dnd-arvisha-4-8-30 dnd-arvisha-4-8-30 8192 30 4 
+nova flavor-create dnd-arvisha-4-8-40 dnd-arvisha-4-8-40 8192 40 4 
+nova flavor-create dnd-arvisha-8-16-30 dnd-arvisha-8-16-30 16384 30 8
+nova flavor-create dnd-arvisha-8-16-40 dnd-arvisha-8-16-40 16384 40 8 
